ec11675d0128db8b1bed2a236052ce9d718d2616
Sergei Klimenkov <kklimser@gmail.com>
2022-08-06 20:13:09 +0300

    new

--- decision_structure_and_boolean_logic/book_club_glasses.py
+++ decision_structure_and_boolean_logic/book_club_glasses.py
@@ -1,10 +1,10 @@
 book = int(input("Введите кол-во книг: "))
 
 if book <= 6:
-    y = book * 5
-    print(f'Вы получили {y} очков')
+    t = book * 5
+    print(f'Вы получили {t} очков')
 elif 7 <= book:
-    y = (book * 60) / book
-    print(f"Вы получаете {y} очков")
+    t = (book * 60) / book
+    print(f"Вы получаете {t} очков")
 else:
     print('Ошибка ввода ')
